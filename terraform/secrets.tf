b16648c52b7b5508cbb9352ec418eecace88c9ac
gordiemp <mhordiienko@gmail.com>
2021-11-27 12:12:09 +0200

    update jenkins

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -1,8 +1,8 @@
-resource "aws_secretsmanager_secret" "spr-appv1" {
-  name = "spr-appv1"
+resource "aws_secretsmanager_secret" "spr-appv2" {
+  name = "spr-appv2"
 }
 
-resource "aws_secretsmanager_secret_version" "spr-appv1" {
-  secret_id     = aws_secretsmanager_secret.spr-appv1.id
+resource "aws_secretsmanager_secret_version" "spr-appv2" {
+  secret_id     = aws_secretsmanager_secret.spr-appv2.id
   secret_string = jsonencode(var.secrets)
 }
